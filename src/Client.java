1f664af6c7e8f7675451dbaa291ffd66e92444b7
ThaboKopane <kpntha001@myuct.ac.za>
2018-03-16 17:02:40 +0200

    new Server and new Client, clientThread gone

--- src/Client.java
+++ src/Client.java
@@ -1,85 +1,126 @@
-import org.omg.Messaging.SYNC_WITH_TRANSPORT;
-
 import java.io.*;
 import java.net.*;
-import java.util.Vector;
 
 public class Client{
-
-
-    static Vector<ClientThread> connectionsVector = new Vector<>();
-    private static BufferedReader bufR =  null;
-    private static ObjectOutputStream output = null;
-    private static ObjectInputStream input = null;
-    private static Socket clientSocket = null;
-    private static boolean closed = false;
-
-
-    public static final int PORT = 8888;
-    public static void main(String[] args) throws Exception{
-        String host = "localhost";
-        Client client = new Client();
-
-        String sentence = null;
-
-        try{
-            clientSocket = new Socket(host, PORT);
-            bufR = new BufferedReader(new InputStreamReader(System.in));
-            output = new ObjectOutputStream(clientSocket.getOutputStream());
-            input = new ObjectInputStream(clientSocket.getInputStream());
-
-            System.out.println("Type in something");
-
-
-            if(clientSocket !=null && output !=null && input !=null){
-                while(true){
-                    System.out.println("writing something to server");
-                    output.writeUTF(bufR.readLine().trim());
-                    output.flush();
-
-                    System.out.println("message: "+input.readUTF());
-                }
-            }
-
-
-        } catch (UnknownHostException un){System.err.println("host unknown");}
-
-
-
-    }
-
-    public void run() {
-        String responseLine;
-        try {
-            while ((responseLine = input.readUTF()) != null) {
-                System.out.println(responseLine);
-                if (responseLine.indexOf("quit") != -1)
-                    break;
-            }
-            closed = true;
-        } catch (IOException ioerr) {
-            System.out.println("IOE: " + ioerr);
-        }
-    }
-
-    public void SendToServer(String msg) throws Exception{
-        //create output stream attached to socket
-        ObjectOutputStream outToServer = new ObjectOutputStream(clientSocket.getOutputStream());
-        //send msg to server
-        outToServer.writeUTF(msg + '\n');
-        outToServer.flush();
-    }
-    public String RecieveFromServer() throws Exception{
-        //create input stream attached to socket
-        ObjectInputStream inFromServer = new ObjectInputStream(clientSocket.getInputStream());
-        //read line from server
-        String res = inFromServer.readUTF();// if connection closes on server end, this throws java.net.SocketException
-        return res;
-    }
-    void close() throws IOException{
-        clientSocket.close();
-    }
+	static String IDENTIFY_NUMBER;
+	static int CONNECT_PORT;
+	Socket socket;
+	ObjectOutputStream out;
+	ObjectInputStream in;
+	String str;
+	private String FILE_TO_RECEIVED;
+	private String STORED_FILE; 
+	public static void main(String[] args) {
+		IDENTIFY_NUMBER = args[0];
+		CONNECT_PORT = Integer.valueOf(args[1]);
+		mkDir(IDENTIFY_NUMBER);
+		new Client().begin();
+	}
+	void begin() {
+		try{
+			socket = new Socket("localhost",CONNECT_PORT);
+			//initialize inputStream and outputStream
+			out = new ObjectOutputStream(socket.getOutputStream());
+			out.flush();
+			//get Input from standard input
+			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+			System.out.println("Please enter command as a string ");
+			Server ser = new Server();
+			new Thread(ser).start();
+			while(true){
+				str = bufferedReader.readLine();//read a sentence from the standard input
+				sendMessage(str);//Send the sentence to the server
+				System.out.println("Meassage sent");
+				System.out.println("Please enter command as a string ");
+			}
+		}
+		catch (ConnectException e) {
+    			System.err.println("Connection refused. You need to initiate a server first.");
+		} 
+		catch(UnknownHostException unknownHost){
+			System.err.println("You are trying to connect to an unknown host!");
+		}
+		catch(IOException ioException){
+			ioException.printStackTrace();
+		}
+		finally{ //Close connections
+			try{
+				in.close();
+				out.close();
+				socket.close();
+			}
+			catch(IOException ioException){
+				ioException.printStackTrace();
+			}
+		}
+	}
+	private class Server implements Runnable {
+		private boolean bconnected = false;
+		public void run() { //receive message from server;
+			try {
+				in = new ObjectInputStream(socket.getInputStream());
+				bconnected = true;
+				while (bconnected) {
+					String inStr = (String)in.readObject();
+					if(!inStr.equals("file")) {
+						System.out.println(inStr);
+					}else {
+						String senderNumber=(String)in.readObject();
+						STORED_FILE = (String)in.readObject();
+						//create file to received path;
+						FILE_TO_RECEIVED = IDENTIFY_NUMBER + "/" + STORED_FILE;	
+				        byte[] contents = new byte[10000];
+				        //Initialize the FileOutputStream to the output file's full path.
+				        FileOutputStream fos = new FileOutputStream(FILE_TO_RECEIVED);
+				        BufferedOutputStream bos = new BufferedOutputStream(fos); 
+				        InputStream is = socket.getInputStream();
+				        String inLengthStr = (String)in.readObject();
+				        int fileLength= Integer.valueOf(inLengthStr); 
+				        int bytesRead = 0;
+				        int[] total=new int[1];
+				       while((bytesRead=is.read(contents))!=-1){
+				    	     total[0]= total[0]+bytesRead;
+				    	     bos.write(contents, 0, bytesRead);
+				    	     if(total[0]==fileLength){
+				    	    	 break;
+				    	     }
+				        }
+				       bos.flush(); 
+				       System.out.println("File: "+STORED_FILE+" was sent by client"+senderNumber);
+					}		        
+				}
+			} catch (SocketException e1) {
+				System.out.println("Bye!");
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+ 
+		}
+	}
+	//send a message to the output stream
+	void sendMessage(String msg)
+	{
+		try{
+			//stream write the message
+			out.writeObject(msg);
+			out.flush();
+		}
+		catch(IOException ioException){
+			ioException.printStackTrace();
+		}
+	}
+	/*Create directory if new client is created*/
+	private static boolean mkDir(String name) {
+		File dir = new File(name);
+		if(dir.exists()) {
+			return false;
+		}
+		if(dir.mkdir()) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
-
-
-                       
--- src/Server.java
+++ src/Server.java
@@ -1,60 +1,322 @@
-import java.io.*;
 import java.net.*;
+import java.io.*;
+//import java.util.Hashmap;
+//import java.util.Observable;
+//import java.util.StringTokenizer;
+//import java.util.Vector;
 import java.util.*;
 
-public class Server {
-    public static final int PORT = 8888;
-
-
-
-
-
-
-    //Vector for active clients
+public class Server{
+    public static int PORT = 8888; //default
+    
+    public static int OTHERPORT; //for me
+    String FILE;
+    /*
     static Vector<ClientThread> connectionsVector = new Vector<>();
     public static int clientCounter = 0;
-    private static ClientDetails userDetails;
+    private static ClientThread[] threads = new ClientThread[15];
     private static Socket clientSocket = null;
     private static ServerSocket serverSocket = null;
-    private static ObjectOutputStream output;
-    private static ArrayList<ObjectOutputStream> clientArray;
+    */
+    
+    static boolean isStarted = false;
+    ServerSocket serverSocket;
+    int clientCount = 1;
+    List<Client> clientList = new ArrayList<Client>(); //figured this is just simpler, but feel free
+    
+    public static void main(String[] args){
+        PORT = 8888; //functionality to change it here instead of just == 8888 (default)
+        new Server().serverGo();
+        }//end main
+    
+    
+        
+    public void serverGo(){
+        try{
+            serverSocket = new ServerSocket(PORT);
+            isStarted = true;
+            }catch(Exception e){
+                System.out.println("Error with the port, I guess.");
+                System.exit(0);
+                }
+        
+        try{
+            while(isStarted){
+                Socket sock = serverSocket.accept();
+                Client client = new Client(sock);
+                new Thread(client).start();
+                clientList.add(client);
+                clientCount++;
+                }
+                }catch(Exception er){er.printStackTrace();}
+                
+            finally{
+                try{serverSocket.close();}catch(Exception someError){someError.printStackTrace();}
+                
+                        
+        }//end serverGo
+    
+    //set/getters  + misc (might not use all, necessarily)
+	/*
+    public static void setStart(){ isStarted = true;}    
+    
+    public boolean getStart(){ return isStarted;}
+    
+    public void printClientList(){
+        for (Client c : clientList)
+            System.out.println(c);
+            }
+    public List<Client> getClientList(){return clientList;}
+            
+    public int getPort(){return PORT;}
+    
+    public static void setPort(int p){ PORT = p;}           
+    */
+    }//end Server class
 
-    public static void main(String args[]) throws IOException {
-        //Server serve = new  Server(PORT);
-        ServerSocket serverSocket = new ServerSocket(PORT);
 
-
-        /*String clientInput;
-        String name ="";
-        serverOutput.writeUTF("Enter your name");*/
-        while (true) {
-            try{
-                clientSocket = serverSocket.accept();
-                clientArray = new ArrayList<>();
-
-
-
-                System.out.println("New Client received : " + clientSocket);
-
-                ClientThread connection = new ClientThread(clientSocket);
-
-                //This is how a thread should be initiated
-                Thread handleMe = new Thread(connection);
-                handleMe.start();
-
-
-                output = new ObjectOutputStream(clientSocket.getOutputStream());
-                clientArray.add(output);
-                break;
-
-            } catch (Exception eror){eror.printStackTrace();}
-
-            System.out.println(connectionsVector.iterator());
-
-        }
-    }
-}
-
-
-
-
+    class Client implements Runnable{
+        private boolean isConnected; //= false;
+        private Socket socket;
+        
+        private ObjectInputStream input;
+        private ObjectOutputStream output;
+        
+        private int num = clientCount;
+        String data;
+        
+            public Client(Socket socket){
+                this.socket = socket;}
+                
+            public void sendMsg(String msg){
+                try{
+                    output.writeObject(msg);
+                    output.flush();
+                    }catch(Exception e){e.printStackTrace();//clientList.remove(this); } 
+                    }    
+                    
+                }//end sendMsg
+                
+                
+                
+            public void sendFile(String path){ //no idea if this'll work. Going by examples/my ass
+                try{
+                    File file = new File(path);
+                    FileInputStream fileIS = new FileInputStream(file);
+                    BufferedInputStream bufIS = new BufferedInputStream(fileIS);
+                    OutputStream outS = socket.getOutputStream();
+                    
+                    
+                    byte[] stuff;
+                    long fileSize = file.length();
+                    String fileClient = Long.toString(fileSize);
+                    sendMsg(fileClient); //send fileSize back to client            Thurs 830
+                    long curr = 0;
+                    
+                    while(curr != fileSize){
+                        int size = 10000;
+                        if((fileSize - curr) >= size){curr = fileSize;}
+                        else{
+                            size = (int)(fileSize - curr);
+                            curr = fileSize;}
+                            
+                        stuff = new byte[size];
+                        bufIS.read(stuff, 0, size);
+                        outS.write(stuff); 
+                    }//end curr/fileSize while
+                       outS.flush();
+                  }catch(Exception e){e.printStackTrace(); //clientList.remove(this);
+                      }
+              }//end sendFile
+              public void run(){
+              
+                  try{
+                     input = new ObjectInputStream(socket.getInputStream());
+                     output = new ObjectOutputStream(socket.getOutputStream());
+                     output.flush();
+                     
+                     isConnected = true;
+                     
+                     while(isConnected){
+                        data = (String)input.readObject();
+                        String[] tempArray = data.split("\\s+");
+                        String whoGets = tempArray[0];
+                        String type = tempArray[1];
+                        
+                        if (type.equals("f")){ //FILE METHOD
+                           FILE = tempArray[2].substring(1, tempArray.length - 1);
+                        }
+                        
+                        String trueMessage = findContent(data); //findContent method to do later
+                        if (whoGets.equals("bc")){ //BROADCOST METHOD
+                           if (type.equals("message")){
+                              for (int i = 0; i < clientList.size(); i++){
+                                 if (i == num-1){
+                                    continue;
+                                    } 
+                                    Client cl = clientList.get(i);
+                                    cl.sendMsg("@client" + num + ":" + trueMessage);
+                                   }
+                                  }
+                              if (type.equals("f")){
+                                 for (int i = 0; i<clientList.size();i++){
+                                    if (i==num-1){
+                                       continue;
+                                       }
+                                    Client cl = clientList.get(i);
+                                    cl.sendMsg("file: ");   
+                                    cl.sendMsg(Integer.toString(num)); 
+                                    cl.sendMsg(extractFileName(tempArray[2]));
+                                    cl.sendFile(FILE); //send the file, no idea if this'll work but whatever
+                                    }
+                                    }
+                                    
+                                    System.out.println("Client " + num + " " + whoGets + " " + type);
+                                    
+                                    }else if(type.equals("pm")){ //else if for the type.equals bc if; pm = private message PRIVATE MESSAGE
+                                       int target = 0;
+                                       if (type.equals("message")){
+                                          String client = tempArray[tempArray.length-1];
+                                          target = getTarget(client);
+                                          for(int i = 0; i<clientList.size();i++){
+                                             if (i==target){
+                                                Client c = clientList.get(i);
+                                                c.sendMsg("@client" + num + ":" + trueMessage);
+                                                
+                                             
+                                            }
+                                         }
+                                      }
+                                      
+                                      if (type.equals("f")){
+                                          String client = tempArray[tempArray.length-1];
+                                          target = getTarget(client);
+                                          for (int i = 0; i<clientList.size(); i++){
+                                             if (target == i){
+                                                Client c = clientList.get(i);
+                                                c.sendMsg("file: ");
+                                                c.sendMsg(Integer.toString(num));
+                                                c.sendMsg(extractFileName(tempArray[2]));
+                                                c.sendFile(FILE);
+                                                }
+                                         }
+                                         
+                                         
+                                     }
+                                     
+                                     int rcv_Client = target + 1;
+                                     System.out.println("Client" + num + " " + whoGets + " " + type + "to client" +rcv_Client + "]");
+                                     
+                                     
+                                    }else if(whoGets.equals("mc")){    //MULTICAST FUNC, kinda: send all, except someone.
+                                    
+                                       int target = 0;
+                                       if (type.equals("message")){
+                                          String client = tempArray[tempArray.length - 1];
+                                          target = getTarget(client);
+                                          for (int i = 0; i<clientList.size(); i++){
+                                             if (target == i || i == num-1){
+                                                continue;
+                                             }
+                                             Client c = clientList.get(i);
+                                             c.sendMsg("@client" + num + ":" + trueMessage);
+                                          }
+                                          
+                                        }
+                                        
+                                        if (type.equals("f")){
+                                          String client = tempArray[tempArray.length - 1];
+                                          target = getTarget(client);
+                                          for(int i=0; i<clientList.size(); i++){
+                                             if (target == i || i == num-1){
+                                                continue;
+                                                }
+                                             Client c = clientList.get(i);
+                                             c.sendMsg("file: ");
+                                             c.sendMsg(Integer.toString(num));
+                                             c.sendMsg(extractFileName(tempArray[2]));
+                                             c.sendFile(FILE);
+                                           }
+                                           
+                                        }
+                                        
+                                       int notClient = target+1;
+                                       System.out.println("client"+ num +" " + whoGets + " " + type +" excluding [client" + notClient + "]");
+                                     } else{   //else for mc if
+                                         System.out.println("error wtf alex");
+                                     }
+                                  }//close the biggest fucking while loop
+                           }catch(Exception e){
+                              clientList.remove(this);
+                              clientCount--;
+                              System.out.println("Client " + this.num + " was disconnected");
+                
+                              }finally{
+                                 try{
+                                    if(output!= null){
+                                       output.close();
+                                       }
+                                    }catch(Exception e){e.printStackTrace();}
+                                    
+                                    if (input!=null){
+                                       try{
+                                          input.close();
+                                          }catch(Exception e){e.printStackTrace();}
+                                          }
+                                    if (socket!=null){
+                                       try{
+                                          socket.close();
+                                          socket = null;
+                                          }catch(Exception e){e.printStackTrace();}
+                                       }
+                                       
+                                       } 
+                                       
+                                     }//end run
+                           
+              
+                  //misc methods
+                  
+                  
+                  
+                  
+                  public String findContent(String data){
+                     int tracker = 0;
+                     while(data.charAt(tracker) != '\"'){
+                        tracker++;
+                        }
+                     int start = tracker++;
+                     
+                     while(data.charAt(tracker)!= '\"'){
+                        tracker++;
+                        }
+                     int end = tracker++;
+                     String trueMessage = data.substring(start+1, end);
+                     return trueMessage;
+                     }
+                     
+                     
+                     }//client class
+                     
+                 public String extractFileName(String filename){
+                     String[] tempB = filename.split("/");
+                     
+                     String tempStr = tempB[tempB.length - 1];
+                     if (tempStr.charAt(0) == '"'){
+                        return tempStr.substring(1,tempStr.length() - 1);
+                     }else{
+                        return tempStr.substring(0, tempStr.length() - 1);
+                       }
+                       
+                   }
+                   
+                   
+                   
+                 public int getTarget(String client){
+                        return (client.charAt(client.length() - 1) - '0') - 1;
+                     }
+                     
+               
+               }//end Server class
+               
+            
